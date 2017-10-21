8515d0aa82b1c245a6fe26135b316fb9ffa683ef
Valerio Sevilla <valerio.sevilla@gmail.com>
2017-10-21 16:01:11 +0200

    ADD ColorManager ADD First implementation of the color stuff

---
+++ TetrisCircular/Assets/Provisional/Script/ColorManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour {
+
+	public enum BlockColor {
+		GREEN,
+		RED,
+		BLUE,
+		YELLOW
+	}
+
+	public Sprite GreenBlock;
+	public Sprite RedBlock;
+	public Sprite BlueBlock;
+	public Sprite YellowBlock;
+
+	public static Dictionary<BlockColor, Sprite> _spritesByColor;
+	public static Dictionary<BlockColor, Sprite> SpritesByColor {
+		get { return _spritesByColor; }
+		private set { _spritesByColor = value; }
+	}
+
+	public static ColorManager _instance;
+	public static ColorManager Instance {
+		get { return _instance; }
+		private set { _instance = value; } 
+	}
+
+	void Awake () {
+		if (Instance == null) {
+			Instance = this;
+			DontDestroyOnLoad (gameObject);
+
+			SpritesByColor = new Dictionary<BlockColor, Sprite> {
+				{ BlockColor.GREEN, GreenBlock },
+				{ BlockColor.RED, RedBlock },
+				{ BlockColor.BLUE, BlueBlock },
+				{ BlockColor.YELLOW, YellowBlock }
+			};
+		} else
+			Destroy (gameObject);
+	}
+}

33ee66e5630d93e9f82f42d8393b9cd33be9fa38
JordiLSG <32976998+JordiLSG@users.noreply.github.com>
2017-10-21 19:05:03 +0200

    Añidido El UI para construir

---
+++ TetrisCircular/Assets/Provisional/Script/ConstructorScripts/TetrominosList.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrominosList : MonoBehaviour
+{
+
+	// Use this for initialization
+	[System.Serializable]
+	public class MinoPosInfo
+	{
+
+		public Vector2 mino1;
+
+
+		public Vector2 mino2;
+
+
+
+		public Vector2 mino3;
+
+
+		public Vector2 mino4;
+
+
+		public string ColorAsignado;
+
+
+		public Color Color;
+
+		public int[] MinoReady;
+
+		public int NumActived = 0;
+
+	}
+
+	public  List <MinoPosInfo> MinoPos = new List<MinoPosInfo> ();
+
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}

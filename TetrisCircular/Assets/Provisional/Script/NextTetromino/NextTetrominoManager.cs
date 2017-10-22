e744a1503b0368856687fdbd6e8ddf3b3a52a28d
JordiLSG <32976998+JordiLSG@users.noreply.github.com>
2017-10-22 10:13:08 +0200

    Añadido next TetrominoManager

---
+++ TetrisCircular/Assets/Provisional/Script/NextTetromino/NextTetrominoManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextTetrominoManager : MonoBehaviour
+{
+
+	// Use this for initialization
+	public GameObject NextTetromino;
+
+	public Transform NextTetrominoPos;
+
+
+
+	public bool ThereIsATetromino = false;
+
+
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public void NextTetrominoModel (GameObject Tetromino)
+	{
+		NextTetromino = Tetromino;
+		
+		Instantiate (Tetromino, NextTetrominoPos.position, Quaternion.identity);
+
+
+
+		ThereIsATetromino = true;
+
+	}
+
+	public bool CheckIfTetroAvailable ()
+	{
+	
+		bool Available = false;
+
+		if (ThereIsATetromino == true) {
+		
+			Available = true;
+
+			ThereIsATetromino = false;
+
+			return Available;
+		
+		
+		
+		}
+
+
+		return Available;
+	
+	
+	
+	
+	}
+}

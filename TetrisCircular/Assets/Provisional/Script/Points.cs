ef72bcaa3e6b59f96706d3f00379330e586380f5
José Adrián Garrido Puertas <adryx@correo.ugr.es>
2017-10-22 15:21:09 +0200

    Contador de puntos

--- TetrisCircular/Assets/Provisional/Script/Points.cs
+++ TetrisCircular/Assets/Provisional/Script/Points.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class Points : MonoBehaviour {
 
+	public TextMesh counter;
+	public static int puntos;
+
+
 	// Use this for initialization
 	void Start () {
-		
+		puntos = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		counter.GetComponent<TextMesh>().text = puntos.ToString();	
 	}
 }

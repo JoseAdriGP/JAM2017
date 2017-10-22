4deabc8e92725513845c3d753cd30189ad283980
José Adrián Garrido Puertas <adryx@correo.ugr.es>
2017-10-22 12:51:38 +0200

    Prefab Repair

--- TetrisCircular/Assets/Provisional/Script/MaterialPosition.cs
+++ TetrisCircular/Assets/Provisional/Script/MaterialPosition.cs
@@ -18,6 +18,8 @@ public class MaterialPosition : MonoBehaviour {
 
 	public GameObject malla;
 
+	public TextMesh counter;
+
 	private List<int> positionElements;
 	private List<GameObject> elementList;
 
@@ -138,22 +140,30 @@ public class MaterialPosition : MonoBehaviour {
 			elementList.Clear ();
 			elementNumber = 0;
 		}
+		updateTreeText ();
 	}
 
-
+	// Update the number of trees in the portview
+	void updateTreeText(){
+		counter.GetComponent<TextMesh>().text = elementNumber.ToString();
+	}
 
 	// Use this for initialization
 	void Start () {
 		positionElements = new List<int>();
 		elementList = new List<GameObject>();
 		ElementGenerator(elementNumber);
+		updateTreeText ();
 	}
 
+	// At the init of the program
 	void Awake(){
 		if(elementNumber > 72 || elementNumber <= 0){
 			elementNumber = 72;
 		}
 	}
+
+
 
 	// Update is called once per frame
 	void Update () {
